d9218ebc4ae11bc349ae7b7f52bfd429c3282de3
Lazaro Estrada <lazaromer97@gmail.com>
2021-06-30 00:57:03 -0400

    Add Comment API views

--- apps/core/api.py
+++ apps/core/api.py
@@ -1,8 +1,8 @@
 from rest_framework import generics
 from rest_framework.permissions import AllowAny
 
-from .models import Task
-from .serializers import TaskSerializer
+from .models import Task, Comment
+from .serializers import TaskSerializer, CommentSerializer
 
 
 class TaskList(generics.ListAPIView):
@@ -32,4 +32,34 @@ class TaskUpdate(generics.UpdateAPIView):
 class TaskDelete(generics.DestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    permission_classes = [AllowAny]
+
+
+class CommentList(generics.ListAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [AllowAny]
+
+
+class CommentDetail(generics.RetrieveAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [AllowAny]
+
+
+class CommentCreate(generics.CreateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [AllowAny]
+
+
+class CommentUpdate(generics.UpdateAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
+    permission_classes = [AllowAny]
+
+
+class CommentDelete(generics.DestroyAPIView):
+    queryset = Comment.objects.all()
+    serializer_class = CommentSerializer
     permission_classes = [AllowAny]
--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .api import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from .api import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CommentList, CommentDetail, CommentCreate, CommentUpdate, CommentDelete
 
 urlpatterns = [
     # Task
@@ -8,4 +8,11 @@ urlpatterns = [
     path('tasks/add/', TaskCreate.as_view(), name='task_create'),
     path('tasks/update/<int:pk>/', TaskUpdate.as_view(), name='task_update'),
     path('tasks/delete/<int:pk>/', TaskDelete.as_view(), name='task_delete'),
+
+    # Comment
+    path('comments/', CommentList.as_view(), name='comment_list'),
+    path('comments/<int:pk>/', CommentDetail.as_view(), name='comment_detail'),
+    path('comments/add/', CommentCreate.as_view(), name='comment_create'),
+    path('comments/update/<int:pk>/', CommentUpdate.as_view(), name='comment_update'),
+    path('comments/delete/<int:pk>/', CommentDelete.as_view(), name='comment_delete'),
 ]
