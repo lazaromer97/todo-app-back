09e9cceda85eacb26cb1be3e2171e9bcd08b85fa
Lazaro Estrada <lazaromer97@gmail.com>
2021-06-30 00:50:26 -0400

    Create CommentSerializer.

--- apps/core/serializers.py
+++ apps/core/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework.serializers import ModelSerializer
 
-from .models import Task
+from .models import Task, Comment
 
 
 class TaskSerializer(ModelSerializer):
     class Meta:
         model = Task
         fields = ['id', 'text', 'completed']
+
+
+class CommentSerializer(ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'task', 'text']
