de3a78b24c383a9faa606d629a43ba37aa08894f
Lazaro Estrada <lazaromer97@gmail.com>
2021-06-30 00:47:32 -0400

    Create Comment model. Add Comment to Django Admin. Make migrations.

--- apps/core/models.py
+++ apps/core/models.py
@@ -4,13 +4,26 @@ from django.db import models
 
 
 class Task(models.Model):
-    text = models.CharField(verbose_name='Name', max_length=120, unique=True)
+    text = models.CharField(verbose_name='Text', max_length=120, unique=True)
     completed = models.BooleanField(verbose_name='Completed', default=False)
 
     class Meta:
         db_table = 'task'
         verbose_name = 'Task'
         verbose_name_plural = 'Tasks'
+
+    def __str__(self):
+        return self.text
+
+
+class Comment(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    text = models.CharField(verbose_name='Text', max_length=255)
+
+    class Meta:
+        db_table = 'comment'
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
 
     def __str__(self):
         return self.text
