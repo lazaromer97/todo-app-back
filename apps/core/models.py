3f755ecc2b018675a693f47cc4f74e48823cded6
Lazaro Estrada <lazaromer97@gmail.com>
2021-06-29 22:14:49 -0400

    Create Task model. Add Task to Django Admin. Make migrations.

--- apps/core/models.py
+++ apps/core/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Task(models.Model):
+    name = models.CharField(verbose_name='Name', max_length=120, unique=True)
+    description = models.TextField(verbose_name='Description')
+    completed = models.BooleanField(verbose_name='Completed', default=False)
+
+    class Meta:
+        db_table = 'task'
+        verbose_name = 'Task'
+        verbose_name_plural = 'Tasks'
+
+    def __str__(self):
+        return self.name
